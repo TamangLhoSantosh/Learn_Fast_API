8cedff5395b950c1146e82c30b7a97393092dab1
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-09-11 22:37:33 +0545

    path and query parameters

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+# from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"data": { "name": "Samtosh"}}
+@app.get('/blog')
+def index(limit=10, published:bool = True):
+        #    sort = Optional[str] = None):
+    # Only get 10 published blogs
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
+    
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : {'all unpublished blogs'}}
+
+@app.get('/blog/{id}')
+def show ( id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit = 10):
+    # Fetch comments of blog with id = id
+    return {'data':{'1','2'}}
