160dc36c037ca2d64d5d0d301c19c40eb369ea79
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-09-12 18:21:14 +0545

    add data to database

--- blog/database.py
+++ blog/database.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False})
 
-SessionLocal = sessionmaker(autocommit = True,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autoflush=False,bind=engine)
 
 Base = declarative_base()
