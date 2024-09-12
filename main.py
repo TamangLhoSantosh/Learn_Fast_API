5df5da9303117e68262b12adbf26b9d521a6d78c
TamangLhoSantosh <sureshtama.g12@gmail.com>
2024-09-12 17:25:17 +0545

    post method

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-# from typing import Optional
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/blog')
-def index(limit=10, published:bool = True):
-        #    sort = Optional[str] = None):
+def index(limit=10, published:bool = True, sort : Optional[str] = None):
     # Only get 10 published blogs
     if published:
         return {'data':f'{limit} published blogs from the db'}
@@ -24,3 +24,14 @@ def show ( id:int):
 def comments(id,limit = 10):
     # Fetch comments of blog with id = id
     return {'data':{'1','2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body:str
+    published_at: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog : Blog):
+    return {'data':f'Blog is created with title as {blog.title}'}
